c2bc40a9a23cb6f7224f830818cd0b7ba65afc39
rudeboybert <albert.ys.kim@gmail.com>
2019-05-23 10:52:13 -0700

    Updated pdf

--- static/talk/2019-05-23_CalPoly.R
+++ static/talk/2019-05-23_CalPoly.R
@@ -1,9 +1,13 @@
 library(tidyverse)
+# Install version 0.2.0.9000 or higher
+# devtools::install_github("moderndive/moderndive")
 library(moderndive)
-library(infer)
+
 
 # Chapter 8: "Virtual" sampling from a bowl of red/white balls
+# Population:
 bowl
+# Sample from population:
 bowl %>% 
   rep_sample_n(size = 50, replace = FALSE, reps = 1)
 
@@ -18,9 +22,12 @@ bowl %>%
 
 
 
+
 # Chapter 9: "Virtual" resampling from a sample of 50 pennies
+# Sample:
 pennies_sample_2
-bowl %>% 
+# Resample from sample:
+pennies_sample_2 %>% 
   rep_sample_n(size = 50, replace = TRUE, reps = 1)
 
 
@@ -32,19 +39,29 @@ bowl %>%
 
 
 
+
+
+
+
+
+
+
 # Chapter 10: Constructing a null distribution from scratch
+library(infer)
+
 null_distribution <- 
-  # Diffs in prop'n promoted: male - female = 87.5% - 58.3% = 29.2%
+  # Diffs in prop'n promoted = male - female = 87.5% - 58.3% = 29.2%:
   promotions %>% 
-  # Variables of interest from data frame
+  # Variables of interest from data frame:
   specify(formula = decision ~ gender, success = "promoted") %>% 
-  # H0 of no difference
+  # H0 of no difference:
   hypothesize(null = "independence") %>% 
-  # Simulate replicate data under H0
+  # Simulate replicate data under H0:
   generate(reps = 1000, type = "permute") %>% 
-  # Summary statistic
+  # Summary statistic:
   calculate(stat = "diff in props", order = c("male", "female"))
 
+# Plot it!
 ggplot(null_distribution, aes(x=stat)) +
   geom_histogram(bins = 10) +
   labs(x = "Diff in proportion", title = "Null distribution") +
